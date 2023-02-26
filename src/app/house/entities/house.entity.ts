71a6ebcf59ffe1467bda536417375510441b5567
Luca Palmarucci <luca.palmarucci@orbyta.it>
2023-02-26 15:10:20 +0100

    House entity - Changed houseId to PrimaryGeneratedColumn

--- src/app/house/entities/house.entity.ts
+++ src/app/house/entities/house.entity.ts
@@ -1,9 +1,14 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { randomUUID } from 'crypto';
 
 @Entity()
 export class House {
-  @PrimaryColumn({ type: 'varchar', default: randomUUID() })
+  @PrimaryGeneratedColumn()
   houseId: string;
 
   @Column({ type: 'varchar', unique: true })
