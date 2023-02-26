cafc14ffbc3c65030e2eef6b1e395d28ffb7f005
Luca Palmarucci <luca.palmarucci@orbyta.it>
2023-02-26 14:36:10 +0100

    Migrate from MongoDB to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './app/authentication/authentication.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { UserModule } from './app/user/user.module';
+import { HouseModule } from './app/house/house.module';
 
 @Module({
   imports: [
@@ -13,19 +14,25 @@ import { UserModule } from './app/user/user.module';
       isGlobal: true,
       envFilePath: ['./src/envs/.env.development'],
     }),
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: process.env.DATABASE_URL,
-      database: process.env.DATABASE_NAME,
-      logging: true,
-      autoLoadEntities: true,
-      useNewUrlParser: true,
-      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
-      useUnifiedTopology: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        database: configService.get('DATABASE_NAME'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        logging: true,
+        useNewUrlParser: true,
+        entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+        synchronize: true,
+      }),
     }),
     UserModule,
     AuthenticationModule,
+    HouseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
