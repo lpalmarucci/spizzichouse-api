d74e2616e4c0cccdac85e6b57b82666e7fea8832
Luca Palmarucci <lpalmarucci@gmail.com>
2023-12-06 18:55:32 +0100

    Setup user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       }),
     }),
     ConfigModule.forRoot(),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
