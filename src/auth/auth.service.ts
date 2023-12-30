2b4da6ec55e64eed57d8ca4cffcbd75e977eab7c
Luca Palmarucci <lpalmarucci@gmail.com>
2023-12-30 17:30:41 +0100

    Added information to jwt payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,18 +3,17 @@ import { UserService } from '@/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { ICryptConfig } from '@/config/types';
 
 @Injectable()
 export class AuthService {
-  private readonly _saltOrRounds: number;
+  private readonly _jwtSettings: ICryptConfig;
   constructor(
     private readonly _configService: ConfigService,
     private readonly _userService: UserService,
     private readonly _jwtService: JwtService,
   ) {
-    this._saltOrRounds = this._configService.get<number>(
-      'config.crypt.saltOrRounds',
-    );
+    this._jwtSettings = this._configService.get<ICryptConfig>('config.crypt');
   }
 
   async signIn(username: string, pass: string): Promise<any> {
@@ -26,6 +25,10 @@ export class AuthService {
     const payload = { sub: user.id, username: user.username };
     return {
       access_token: this._jwtService.sign(payload),
+      firstname: user.firstname,
+      lastname: user.lastname,
+      username: user.username,
+      expiresIn: this._jwtSettings.jwtExpiration,
     };
   }
 }
