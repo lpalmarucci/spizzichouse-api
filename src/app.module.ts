3d1088425299f6cb4052c25eed60977fd839bc2b
Luca Palmarucci <lpalmarucci@gmail.com>
2024-06-18 15:41:43 +0200

    get databse configurations from config service instead of process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '@/user/user.module';
 import { LocationModule } from './location/location.module';
 import { MatchModule } from './match/match.module';
@@ -10,29 +9,38 @@ import { RoundModule } from './round/round.module';
 import { AuthModule } from './auth/auth.module';
 import { MatchHistoryModule } from './match-history/match-history.module';
 import config from '@/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IDatabaseConfig } from '@/config/types';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        schema: process.env.DATABASE_SCHEMA,
-        logging: process.env.NODE_ENV === 'development',
-        autoLoadEntities: true,
-        entities: ['src/**/*{.entity.ts}'],
-        synchronize: process.env.NODE_ENV === 'development',
-        migrations: ['./dist/migrations/*'],
-        migrationsRun: process.env.NODE_ENV !== 'development',
-      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig = configService.get<IDatabaseConfig>('config.database');
+        const env = configService.get<string>('env');
+        return {
+          type: 'postgres',
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.user,
+          password: dbConfig.password,
+          database: dbConfig.name,
+          schema: dbConfig.schema,
+          logging: env === 'development',
+          autoLoadEntities: true,
+          entities: ['src/**/*{.entity.ts}'],
+          synchronize: env === 'development',
+          migrations: ['./dist/migrations/*'],
+          migrationsRun: env !== 'development',
+        };
+      },
     }),
     ConfigModule.forRoot({
       load: [config],
     }),
+    // DatasourceModule,
     UserModule,
     LocationModule,
     MatchModule,
